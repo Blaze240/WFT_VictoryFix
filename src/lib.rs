ff4b8cc336d0be252de86fed55db4aef638d058c
Jensen Nguyen <likeaboss24000@gmail.com>
2024-04-15 12:49:13 -0400

    Added code for Dair

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,11 @@
 
 mod chrom_fsmash;
 mod chrom_fair;
+mod chrom_dair;
 
 #[skyline::main(name = "smashline_test")]
 pub fn main() {
     chrom_fsmash::install();
     chrom_fair::install();
+    chrom_dair::install();
 }
