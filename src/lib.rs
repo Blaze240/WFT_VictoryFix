84dedd193bb2d0c6b3f3b173061715a6300281b6
Jensen Nguyen <likeaboss24000@gmail.com>
2024-04-16 21:39:18 -0400

    Added code for Back Air and Down Smash

--- src/lib.rs
+++ src/lib.rs
@@ -7,14 +7,18 @@
 )]
 
 mod chrom_fsmash;
+mod chrom_dsmash;
 mod chrom_fair;
 mod chrom_dair;
 mod chrom_uair;
+mod chrom_bair;
 
 #[skyline::main(name = "kain_moveset")]
 pub fn main() {
     chrom_fsmash::install();
+    chrom_dsmash::install();
     chrom_fair::install();
+    chrom_bair::install();
     chrom_dair::install();
     chrom_uair::install();
 }
